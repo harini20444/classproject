30353e48e261acec086960a9f98ad35c8769bd90
harini20444 <22eg107b44@anurag.edu.in>
2024-08-01 19:03:02 +0530

    first commit

---
+++ app_server/routes/index.js
@@ -0,0 +1,14 @@
+const ctrlMain = require("../controllers/main");
+const express = require("express");
+const router = express.Router();
+const ctrlLocations = require("../controllers/locations");
+const ctrlOthers = require("../controllers/others");
+/* Locations pages */
+router.get("/", ctrlLocations.homelist);
+router.get("/location", ctrlLocations.locationInfo);
+router.get("/location/review/new", ctrlLocations.addReview);
+/* Other pages */
+router.get("/about", ctrlOthers.about);
+/* GET homepage. */
+router.get("/", ctrlMain.index);
+module.exports = router;
